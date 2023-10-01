1497ef9cc770ddd424065d4165d91cc940ecf0cd
Adam Wong <tue91717@temple.edu>
2023-10-01 01:15:03 -0400

    Add path layer to board's tile_map. Implement methods to create, extend, truncate, and clear the path.

---
+++ asset/tile_atlas.gd
@@ -0,0 +1,39 @@
+## TileAtlas stores aliases for interactive tile_atlases.tres resources.
+class_name TileAtlas
+
+
+const tiles = {
+	"SOURCE": 0,
+	"EMPTY": Vector2i(-1, -1),
+	"BLACK": Vector2i(0, 0),
+	"WHITE": Vector2i(1, 0),
+	"RAINBOW": Vector2i(2, 0),
+	"NEUTRAL": Vector2i(3, 0),
+	"OUT": Vector2i(4, 0),
+	"OUT_WARNING": Vector2i(5, 0),
+	"OTHER": Vector2i(6, 0),
+}
+
+
+const path = {
+	"SOURCE": 1,
+	"EMPTY": Vector2i(-1, -1),
+	"NW_CORNER": Vector2i(0, 0),
+	"NORTH": Vector2i(1, 0),
+	"NE_CORNER": Vector2i(2, 0),
+	"WEST": Vector2i(0, 1),
+	"CENTER": Vector2i(1, 1),
+	"EAST": Vector2i(2, 1),
+	"SW_CORNER": Vector2i(0, 2),
+	"SOUTH": Vector2i(1, 2),
+	"SE_CORNER": Vector2i(2, 2),
+	"HORIZONTAL": Vector2i(0, 3),
+	"VERTICAL": Vector2i(1, 3),
+}
+
+
+const anim = {
+	"SOURCE": 2,
+	"EMPTY": Vector2i(-1, -1),
+	"PATH_END": Vector2i(0, 0),
+}
--- src/scene/board/tile_map.gd
+++ src/scene/board/tile_map.gd
@@ -1,17 +1,132 @@
-## This script controls the TileMap contained by the board scene.
+## This [TileMap] extension handles the drawing of tiles contained in the board scene.
 extends TileMap
 
-## The coordinate of the top-left tile of the pathable region.
+
+## Aliases for each layer.
+enum LAYER { BACKGROUND, ACTIVE_TILES, PATH }
+## The local coordinates of the top-left tile of the in-bounds region.
 @export var top_left = Vector2i(0, 8)
-## The coordinate of the bottom-right tile of the pathable region.
+## The local coordinates of the bottom-right tile of the in-bounds region.
 @export var bottom_right = Vector2i(10, 18)
+var atlas = TileAtlas.new()
+## An array of coordinates in the path layer.
+var path: Array
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	path = []
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+## Adds coords to the path layer.
+## Returns true if the operation is successful.
+func path_append(coords: Vector2i) -> bool:
+	if tile_is_pathable(coords):
+		path.append(coords)
+		_path_update()
+		return true
+	return false
+
+
+## Clears the path layer.
+func path_clear() -> void:
+	self.clear_layer(LAYER.PATH)
+	path.clear()
+
+
+## Truncates the path such that it ends at index.
+func path_truncate(index: int):
+	if not index in range(path.size()):
+		return
+	# Erase tiles from layer.
+	for i in path.slice(index + 1).size():
+			self.set_cell(2, path[i + index + 1], -1)
+	# Truncate path.
+	path = path.slice(0, index + 1)
+	_path_update()
+
+
+## Returns true if coords is in bounds.
+func tile_is_in_bounds(coords: Vector2i) -> bool:
+	return coords.x >= top_left.x and coords.x <= bottom_right.x \
+			and coords.y >= top_left.y and coords.y <= bottom_right.y
+
+
+## Returns true if coords can be added to the path.
+func tile_is_pathable(coords: Vector2i) -> bool:
+	if not tile_is_in_bounds(coords):
+		return false
+	elif path.is_empty():
+		return true
+	elif path.has(coords):
+		return false
+	else:
+		return coords.x == path[-1].x and abs(path[-1].y - coords.y) == 1 \
+				or coords.y == path[-1].y and abs(path[-1].x - coords.x) == 1
+
+
+## Updates the drawing of the last two tiles in the path.
+func _path_update() -> void:
+	if not path.is_empty():
+		if path.size() > 1:
+			# Set the second-to-last tile.
+			var atlas_coords = Vector2i(-1, -1)
+			var to_last = Vector2i(path[-1].x - path[-2].x, path[-1].y - path[-2].y)
+			if path.size() > 2:
+				var to_second_last = Vector2i(path[-2].x - path[-3].x, path[-2].y - path[-3].y)
+				match to_second_last:
+					Vector2i(0, -1):
+						match to_last:
+							Vector2i(-1, 0):
+								atlas_coords = atlas.path.SW_CORNER
+							Vector2i(1, 0):
+								atlas_coords = atlas.path.SE_CORNER
+							_:
+								atlas_coords = atlas.path.VERTICAL
+					Vector2i(-1, 0):
+						match to_last:
+							Vector2i(0, -1):
+								atlas_coords = atlas.path.NE_CORNER
+							Vector2i(0, 1):
+								atlas_coords = atlas.path.SE_CORNER
+							_:
+								atlas_coords = atlas.path.HORIZONTAL
+					Vector2i(1, 0):
+						match to_last:
+							Vector2i(0, -1):
+								atlas_coords = atlas.path.NW_CORNER
+							Vector2i(0, 1):
+								atlas_coords = atlas.path.SW_CORNER
+							_:
+								atlas_coords = atlas.path.HORIZONTAL
+					Vector2i(0, 1):
+						match to_last:
+							Vector2i(-1, 0):
+								atlas_coords = atlas.path.NW_CORNER
+							Vector2i(1, 0):
+								atlas_coords = atlas.path.NE_CORNER
+							_:
+								atlas_coords = atlas.path.VERTICAL
+					_:
+						printerr("Invalid 3rd-to-2nd-last difference vector: ", to_second_last)
+			else:
+				match to_last:
+					Vector2i(0, -1):
+						atlas_coords = atlas.path.NORTH
+					Vector2i(-1, 0):
+						atlas_coords = atlas.path.WEST
+					Vector2i(1, 0):
+						atlas_coords = atlas.path.EAST
+					Vector2i(0, 1):
+						atlas_coords = atlas.path.SOUTH
+					_:
+						printerr("Invalid 2nd-to-last difference vector: ", to_last)
+
+			self.set_cell(2, path[-2], atlas.path.SOURCE, atlas_coords)
+
+		self.set_cell(2, path[-1], atlas.anim.SOURCE, atlas.anim.PATH_END)
