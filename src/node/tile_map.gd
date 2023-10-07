1164a73313c522eddbccf1774a174f6fbc71bb6f
Adam Wong <tue91717@temple.edu>
2023-10-06 20:42:57 -0400

    Split clear_rows() into multiple functions. match_rows() returns matching row counts and matching tiles. clear_tiles() clears an array of tiles in a layer.

--- src/node/tile_map.gd
+++ src/node/tile_map.gd
@@ -20,16 +20,20 @@ func _process(_delta):
 	pass
 
 
-## Removes rows of tiles in the drop layer with matching tiles.
-## 'full_rows_only': If true, then only rows with a full set of tiles are cleared.
-## Returns a dictionary mapping tile atlas coordinates to the number of rows
-## cleared,and mapping Vector2i(-1, -1) to unique row count.
-func clear_rows(full_rows_only = true) -> Dictionary:
+## Reports the number of rows of tiles in 'layer' with matching tiles,
+## and the coordinates of matched tiles.
+## 'full_rows_only': If true, then only rows with a full set of tiles are reported.
+## Returns a dictionary mapping:
+## 1. tile atlas coordinates to the number of rows cleared,
+## 2. Vector2i(-1, -1) to unique row count,
+## 3. Vector2i(-1, -2) to an array of matched tile coordinates.
+func match_rows(layer: int, full_rows_only = true) -> Dictionary:
 	# Maps atlas coordinates to row clears.
 	var counts = {Vector2i(-1, -1): 0}
+	var matched_tiles = []
 	# Maps atlas coordinates to group values.
 	var group_values = {}
-	var tiles = get_used_cells(layers.drop)
+	var tiles = get_used_cells(layer)
 	# Sort tile coordinates from top-to-bottom, then left-to-right.
 	tiles.sort_custom(func(a, b): return a.y < b.y or a.y == b.y and a.x < b.x)
 	for i in range(0, tiles.size(), drop_width):
@@ -37,8 +41,8 @@ func clear_rows(full_rows_only = true) -> Dictionary:
 		# Sum the group values of tiles in the row.
 		var total = 0
 		for j in drop_width:
-			var tile_atlas_coords = get_cell_atlas_coords(layers.drop, tiles[i + j])
-			var tile_data = get_cell_tile_data(layers.drop, tiles[i + j])
+			var tile_atlas_coords = get_cell_atlas_coords(layer, tiles[i + j])
+			var tile_data = get_cell_tile_data(layer, tiles[i + j])
 			if tile_data:
 				var value = tile_data.get_custom_data("group")
 				total = total + value
@@ -55,10 +59,17 @@ func clear_rows(full_rows_only = true) -> Dictionary:
 				counts[atlas_coords] = counts[atlas_coords] + 1
 		if is_matched:
 			counts[Vector2i(-1, -1)] = counts[Vector2i(-1, -1)] + 1
-			# Clear the row.
+			# Add the row to the array.
 			for j in drop_width:
-				set_cell(layers.drop, tiles[i + j], -1)
+				matched_tiles.append(tiles[i + j])
+	counts[Vector2i(-1, -2)] = matched_tiles
 	return counts
+
+
+## Clears the 'tiles' in 'layer'.
+func clear_tiles(layer: int, tiles: Array):
+	for tile in tiles:
+		set_cell(layer, tile, -1)
 
 
 ## Clears the path.
