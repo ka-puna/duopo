fd2419826b26b423017e18f79c0cf222b461611c
Adam Wong <tue91717@temple.edu>
2023-10-08 02:57:28 -0400

    Drop a random pattern every 30 seconds.

--- src/scene/time_mode.gd
+++ src/scene/time_mode.gd
@@ -1,14 +1,36 @@
 extends Control
 
 
+## The period between drops in seconds.
+@export var cycle_period = 30
+var board: TileMapPathable
+var tile_set: TileSet
+var layers: Dictionary
+var atlas
+var commander
+var drop: Callable
+var cycle_time
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	board = $board
+	tile_set = $board.tile_set
+	layers = $board.layers
+	atlas = $board.atlas
+	commander = preload("res://src/node/tile_map/tile_map_command.gd").new(board)
+	drop = commander.get_func_drop_layer()
+	cycle_time = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func _process(delta):
+	cycle_time = cycle_time + delta
+	if cycle_time >= cycle_period:
+		# Add a random pattern to the board.
+		board.add_pattern(layers.drop, randi_range(0, tile_set.get_patterns_count()))
+		drop.call(layers.drop)
+		cycle_time = 0
 
 
 func _on_pause_game_pressed():
