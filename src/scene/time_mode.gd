0aaefd16ca82c0f4d7e7f5091d61de99db594856
Adam Wong <tue91717@temple.edu>
2023-10-14 01:51:50 -0400

    Add pattern_level variable for pattern selection. Pattern level increases every 5 levels.

--- src/scene/time_mode.gd
+++ src/scene/time_mode.gd
@@ -11,6 +11,8 @@ extends CycleModeBase
 @onready var level: int = 0: set = set_level
 @onready var rows_cleared: int = 0: set = set_rows_cleared
 var match_rows: Callable
+var pattern_level: int: set = set_pattern_level
+var patterns: Array
 
 
 # Called when the node enters the scene tree for the first time.
@@ -25,6 +27,7 @@ func _ready():
 	effect = commander.get_path_map(atlas.TILES_SELF_MAPPING)
 	match_rows = commander.get_match_rows("group")
 	preview.init(tile_set, cycle_period)
+	pattern_level = 0
 	var pattern = get_new_pattern()
 	preview.set_pattern_id(pattern)
 
@@ -36,6 +39,20 @@ func _process(delta):
 	if cycle_value >= cycle_period:
 		if not drop_pattern():
 			game_over()
+
+
+func get_new_pattern(repeat = true) -> int:
+	var limit = patterns.size()
+	var random_f = randf_range(0, limit)
+	while random_f == limit:
+		random_f = randf_range(0, limit)
+	var index = floori(random_f)
+	var frac = random_f - index
+	if frac < patterns[index][0] or not repeat:
+		var random_i = randi_range(1, patterns[index].size() - 1)
+		return patterns[index][random_i]
+	else:
+		return get_new_pattern(false)
 
 
 func get_stats() -> Dictionary:
@@ -69,18 +86,71 @@ func set_level(value: int):
 	cycle_period = clampf(new_cycle_period, 1, max_cycle_period)
 
 
+## Set the patterns used by the game according to 'pattern_level'. 
+func set_pattern_level(value: int):
+	pattern_level = value
+	match value:
+		0:
+			# 3-height patterns.
+			patterns = atlas.SAMPLE.slice(0, 6, 1, true)
+		1:
+			# 3-height and 1-height patterns.
+			patterns = atlas.SAMPLE.slice(0, 6, 1, true)
+			patterns.append_array(atlas.SAMPLE.slice(10, 14, 1, true))
+		2:
+			# 3-height and 2-height patterns
+			patterns = atlas.SAMPLE.slice(0, 6, 1, true)
+			patterns.append_array(atlas.SAMPLE.slice(6, 10, 1, true))
+		3:
+			# 3-height and 4-height patterns.
+			patterns = atlas.SAMPLE.slice(0, 6, 1, true)
+			patterns.append_array(atlas.SAMPLE.slice(14, 19, 1, true))
+		4:
+			# 4-height and 2-height patterns.
+			patterns = atlas.SAMPLE.slice(14, 19, 1, true)
+			patterns.append_array(atlas.SAMPLE.slice(6, 10, 1, true))
+		5:
+			# 2-height, 1-height, and 4-height patterns.
+			patterns = atlas.SAMPLE.slice(6, 14, 1, true)
+			patterns.append_array(atlas.SAMPLE.slice(14, 19, 1, true))
+		6:
+			# 3-height patterns.
+			patterns = atlas.SAMPLE.slice(0, 14, 1, true)
+		7:
+			# 3-height and 2-height patterns.
+			patterns = atlas.SAMPLE.slice(0, 6, 1, true)
+			patterns.append_array(atlas.SAMPLE.slice(6, 10, 1, true))
+		8:
+			# 3-height, 2-height, and 1-height patterns.
+			patterns = atlas.SAMPLE.slice(0, 14, 1, true)
+		9:
+			# Full pattern set.
+			patterns = atlas.SAMPLE.duplicate(true)
+		_:
+			set_pattern_level(value % 10)
+	# Correct subset values.
+	var num_patterns = patterns.size()
+	for subset in patterns:
+		if subset[0] < 1:
+			var p = subset[0]
+			subset[0] = (p * num_patterns + p - 1)/(num_patterns + p -1)
+
+
 func set_rows_cleared(value: int):
 	rows_cleared = value
-	update_level()
+	update_levels()
 
 
-func update_level():
+func update_levels():
 	var score_level = floori(score * 0.0004 - level * 0.4)
 	var row_level = floori(rows_cleared * 0.1)
 	var min_level = mini(score_level, row_level)
 	if min_level > level:
 		score += 1000
 		set_level(min_level)
+	var min_pattern_level = floori(min_level * 0.2)
+	if min_pattern_level > pattern_level:
+		set_pattern_level(min_pattern_level)
 
 
 func _on_tile_mouse_event(tile: Vector2i, button: MouseButtonMask, pressed: bool):
